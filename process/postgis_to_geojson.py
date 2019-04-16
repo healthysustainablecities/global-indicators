7c719539b7505f7d180fbac7a14be398c31340da
gboeing <gboeing@berkeley.edu>
2019-04-15 21:23:10 -0400

    remove files

--- process/postgis_to_geojson.py
+++
@@ -1,6 +0,0 @@
-year = 2016
-for locale in ['adelaide','bris','canberra','darwin','hobart','melb','perth','syd']:
-  for feature in ['boundaries_sa1','boundaries_ssc','boundaries_lga','li_map_sa1','li_map_ssc','li_map_lga','ind_description','urban_sos']:
-    print('ogr2ogr -f KML {feature}_{locale}_{year}.kml "PG:host=localhost dbname=observatory user=postgres password=pepe!42" {feature}_{locale}_{year}'.format(feature = feature, locale = locale, year = year))
-  
-  
