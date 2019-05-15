3dc7c94679e05764b2eefcaac46e99e6a56f998a
ShirleyLiu <ShirleyLiu@example.com>
2019-05-15 12:20:24 -0400

    add OSM modules,config file  and notebook

---
+++ process/config.py
@@ -0,0 +1,16 @@
+# Script:  config.py
+# Raw data configuration file
+# Version: 201905
+# Author:  Shirley Liu
+# Description: All scripts within the data process folder draw on the parameters specified in this configeration file to source and output resources. 
+
+
+places = {'phoenix' : 'Phoenix, AZ, USA'} # study region name
+
+time = '_201905' # output data time
+
+buffered = 1e4 #study region buffer 10km
+
+OSM_data_loc = '../data/OSM' #street network raw data output folder where data resources for the project are located
+
+
