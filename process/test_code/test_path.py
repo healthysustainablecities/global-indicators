66a14c3c3451f92b0a0ec97d87c34325c8bd3a9d
zacwang <knde2019@gmail.com>
2019-12-20 00:31:27 +1100

    sv version

--- process/test_code/test_path.py
+++
@@ -1,8 +0,0 @@
-import os
-# os.getcwd(), notice relative path is relatived to current working directory
-
-dirname = os.path.dirname(__file__)
-txtfile = os.path.join(dirname,'../abc.txt')
-print(os.getcwd())
-with open(txtfile) as f:
-    print(f.readlines())
