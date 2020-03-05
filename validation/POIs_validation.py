c22d58875db12c8974fdf6b08808c07caf7fd974
thuanguy <59931574+thuanguy@users.noreply.github.com>
2020-03-05 13:02:21 -0800

    Update POIs_validation.py

--- validation/POIs_validation.py
+++ validation/POIs_validation.py
@@ -8,63 +8,59 @@ OSM_filename = "belfast_gb_2019_1600m_buffer.gpkg"
 POIs_name = "fresh_food_market"
 
 gdf = gpd.read_file(OSM_filename, layer='destinations')
-OSM_points = gdf[gdf['dest_name']==POIs_name]
+gdf_osm = gdf[gdf['dest_name']==POIs_name]
 
-OPoints = gpd.GeoDataFrame.from_file(official_filename)
-print("number of OSM points: " ,len(OSM_points))
-print("number of Official points: ", len(OPoints))
+gdf_official = gpd.GeoDataFrame.from_file(official_filename)
+print("number of OSM points: " ,len(gdf_osm))
+print("number of Official points: ", len(gdf_official))
 
-#Calculate number of OSM_points intersect with OPoints
-mask1 = OSM_points['geometry'].intersects(OPoints['geometry'].unary_union)
+#Project gdf_osm to the crs of the official layer
+gdf_osm = gdf_osm.to_crs(gdf_official.crs)
+print(gdf_osm.crs)
+#ax = gdf_osm.plot()
 
-print("number of intersection items: ",len(OSM_points[mask1]))
-print("Percentage: ",len(OSM_points[mask1])*100/len(OSM_points) )
-# #print(len(OSM_points[~mask1]))
+#Calculate number of gdf_osm intersect with gdf_official
+mask1 = gdf_osm['geometry'].intersects(gdf_official['geometry'].unary_union)
 
-# mask2 = OPoints['geometry'].intersects(OSM_points['geometry'].unary_union)
-# print(len(OPoints[mask2]))
+print("number of intersection items: ",len(gdf_osm[mask1]))
+print("Percentage: ",len(gdf_osm[mask1])*100/len(gdf_osm) )
 
-
-#Project OSM_points to the crs of the official layer
-OSM_points = OSM_points.to_crs(OPoints.crs)
-print(OSM_points.crs)
-#ax = OSM_points.plot()
 
 #Draw 100m buffer around OSM points; draw 100m buffer around official points
-OSM_buffer = OSM_points
+OSM_buffer = gdf_osm
 OSM_buffer['geometry'] = OSM_buffer.geometry.buffer(100)
-OP_buffer = OPoints
+OP_buffer =gdf_official
 OP_buffer['geometry'] = OP_buffer.geometry.buffer(100)
 
 #Calculate percent of OSM points within Official points 100m buffer
-mask = OSM_points['geometry'].intersects(OP_buffer['geometry'].unary_union)
-print("intersection: ",len(OSM_points[mask]))
-print("percent of OSM points", str(len(OSM_points[mask])*100/len(OSM_points)))
+mask = gdf_osm['geometry'].intersects(OP_buffer['geometry'].unary_union)
+print("intersection: ",len(gdf_osm[mask]))
+print("percent of OSM points", str(len(gdf_osm[mask])*100/len(gdf_osm)))
 
 #Calculate percent of Official Point within OSM points 100m buffer
-mask = OPoints['geometry'].intersects(OSM_buffer['geometry'].unary_union)
-print("intersection: ",len(OPoints[mask]))
-print("percent of Official Point: ", str(len(OPoints[mask])*100/len(OPoints)))
+mask = gdf_official['geometry'].intersects(OSM_buffer['geometry'].unary_union)
+print("intersection: ",len(gdf_official[mask]))
+print("percent of Official Point: ", str(len(gdf_official[mask])*100/len(gdf_official)))
 
 #Calculate distance to all Official polygons and pick the min
-def min_distance(OSM_points, OPoints):
+def min_distance(gdf_osm, gdf_official):
     nearest_dists = []
-    for item in OSM_points.geometry:
-        nearest_dist = OPoints.distance(item).min()
+    for item in gdf_osm.geometry:
+        nearest_dist = gdf_official.distance(item).min()
         nearest_dists.append(nearest_dist)
-    OSM_points['nearest_distance'] = nearest_dists
-    print(OSM_points['nearest_distance'])
+    gdf_osm['nearest_distance'] = nearest_dists
+    print(gdf_osm['nearest_distance'])
 
 
-min_distance(OSM_points,OPoints)
-#Graphing the histogram of distance from OSM_points to nearest Official points
-ax = OSM_points['nearest_distance'].hist()
+min_distance(gdf_osm,gdf_official)
+#Graphing the histogram of distance from gdf_osm to nearest Official points
+ax = gdf_osm['nearest_distance'].hist()
 plt.show()
 
-mean_dist = OSM_points['nearest_distance'].mean()
+mean_dist = gdf_osm['nearest_distance'].mean()
 print("mean nearest distance: ",mean_dist)
 
-median_dist = OSM_points['nearest_distance'].median()
+median_dist = gdf_osm['nearest_distance'].median()
 print("median nearest distance: ", median_dist)
 
 
