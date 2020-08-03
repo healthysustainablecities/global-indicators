25ad657b0bb8e598a6a0955b042f9ef79e130165
Geoff Boeing <boeing@usc.edu>
2020-08-03 12:03:02 -0700

    fix mount

--- docker-bash.sh
+++ docker-bash.sh
@@ -1,3 +1,3 @@
 docker pull gboeing/global-indicators:latest
 git pull
-docker run --rm -it -v "$PWD$":/home/jovyan/work gboeing/global-indicators /bin/bash
+docker run --rm -it -v "$PWD":/home/jovyan/work gboeing/global-indicators /bin/bash
