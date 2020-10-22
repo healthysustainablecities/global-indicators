920c4a9911bc7db2ca369c7abd73d9bb807e6731
Carl Higgs <carl.higgs@rmit.edu.au>
2020-10-22 14:08:03 +1100

    updated the 'process_region.sh' shell script (although it seems from experience that unix line endings don't preserve for this file, perhaps as I'm commiting from a windows host, unfortunately)

--- process/pre_process/process_region.sh
+++ process/pre_process/process_region.sh
@@ -6,12 +6,14 @@ do
     python 01_create_study_region.py $i
     python 02_create_osm_resources.py $i
     python 03_create_network_resources.py $i
+    python 03_create_network_resources_stop_gap.py $i
     python 04_create_hex_grid.py $i
     python 05_create_population_grid.py $i
     python 06_compile_destinations.py $i
     python 07_open_space_areas_setup.py $i
     python 08_locate_origins_destinations.py $i
     python 09_hex_destination_summary.py $i
-    python 10_osm_audit_pop.py $i
+    python 10_destination_audit.py $i
+    python 11_urban_covariates.py $i
     python _export_gpkg.py $i
 done
