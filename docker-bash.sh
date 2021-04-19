18962dd69565dbaed45e714716b3fea08c7c4d94
Shirley Liu <46453772+shiqin-liu@users.noreply.github.com>
2021-04-19 11:13:19 -0400

    update project docker image

--- docker-bash.sh
+++ docker-bash.sh
@@ -1,3 +1,3 @@
-docker pull gboeing/global-indicators:latest
+docker pull globalhealthyliveablecities/global-indicators:latest
 git pull
-docker run --rm -it --shm-size=2g --net=host -v "$PWD":/home/jovyan/work gboeing/global-indicators /bin/bash
+docker run --rm -it --shm-size=2g --net=host -v "$PWD":/home/jovyan/work globalhealthyliveablecities/global-indicators /bin/bash
