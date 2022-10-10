3ce0ac3bc66b9074eddc6626b6cd1c3213f18d67
Carl Higgs <carl.higgs@rmit.edu.au>
2022-10-10 19:35:14 +1100

    minor refactoring of population grid parameters in preparation for larger later work simplifying and using new population data

--- process/pre_process/10_destination_audit.py
+++ process/pre_process/10_destination_audit.py
@@ -27,9 +27,9 @@ def main():
     date_yyyymmdd = time.strftime("%d%m%Y")
     # population raster set up 
     population_folder = '../data/GHS'
-    # projections = {'Mollwiede':54009,'WGS84':4326}
-    projections = {'WGS84':4326}
-    ghs_year = 2015   
+    epsg = int(df_datasets.loc['population']['epsg'])
+    p = df_datasets.loc['population']['epsg_name']
+    ghs_year = int(df_datasets.loc['population']['year_target'])
     region_sql = '''
     SELECT "Study region" study_region, 
            geom
@@ -69,68 +69,71 @@ def main():
         locale_dir = os.path.join(folder_path,'study_region','{}'.format(study_region))
         buffered_study_region = '{}_{}{}'.format(study_region,study_buffer,units)
         engine = create_engine(f"postgresql://{db_user}:{db_pwd}@{db_host}/{db}")
-    db_contents = inspect(engine)
-        clipping_boundary = gpd.GeoDataFrame.from_postgis('''SELECT geom FROM {table}'''.format(table = buffered_study_region), engine, geom_col='geom' )   
-        for p in projections:
-            # construct virtual raster table
-            vrt = os.path.join(population_folder,f'ghs_{p}.vrt')
-            population_raster_clipped   = os.path.join(locale_dir,f'ghs_{ghs_year}_{p}_{locale}.tif')
-            population_raster_projected = os.path.join(locale_dir,f'ghs_{ghs_year}_{p}_{locale}_{srid}.tif')
-            pop_feature = f'pop_ghs_{ghs_year}'
-            print("Global population dataset..."),
-            if not os.path.isfile(vrt):
-                tif_folder = f'../data/GHS/{p}'
-                tif_files = [os.path.join(tif_folder,file) for file in os.listdir(tif_folder) if os.path.splitext(file)[-1] == '.tif']
-                gdal.BuildVRT(vrt, tif_files)
-                print(f"  has now been indexed ({vrt}).")
-            else:
-                print(f"  has already been indexed ({vrt}).")
-            print("Population data clipped to region..."),
-            if not os.path.isfile(population_raster_clipped):
-                # extract study region boundary in projection of tiles
-                clipping = clipping_boundary.to_crs({'init':'epsg:{}'.format(projections[p])})
-                # get clipping boundary values in required order for gdal translate
-                bbox = list(clipping.bounds[['minx','maxy','maxx','miny']].values[0]) 
-                # bbox = list(clipping.bounds.values[0]) 
-                ds = gdal.Translate(population_raster_clipped, vrt, projWin = bbox)
-                ds = None
-                print(f"  has now been created ({population_raster_clipped}).")
-            else:
-                print(f"  has already been created ({population_raster_clipped}).")
-            print("Population data projected for region..."),
-            if not os.path.isfile(population_raster_projected):
-                # reproject and save the re-projected clipped raster
-                # (see config file for reprojection function)
-                reproject_raster(inpath = population_raster_clipped, 
-                              outpath = population_raster_projected, 
-                              new_crs = 'EPSG:{}'.format(srid))   
-                print(f"  has now been created ({population_raster_projected}).")
-            else:
-                print(f"  has already been created ({population_raster_projected}).")
-            print("Interpolation of population data to urban area...")
-            analysis_area = gpd.GeoDataFrame.from_postgis(region_sql,
-                                                            engine, 
-                                                            geom_col='geom')
-            print("  - processing population zonal statistics...")
-            pop_feature = "urban_study_region_pop"
-            if not db_contents.has_table(pop_feature):
-                result = zonal_stats(analysis_area,population_raster_projected,stats="sum", all_touched=True,geojson_out=True, nodata=-200)
-                print("  - creating additional required fields...")
-                urban_pop = gpd.GeoDataFrame.from_features(result)
-                urban_pop.rename(columns={'sum':'urban_pop_est'},inplace=True)
-                urban_pop['area_sqkm'] = urban_pop['geometry'].area/10**6
-                urban_pop['pop_per_sqkm'] = urban_pop['urban_pop_est'] / urban_pop['area_sqkm']
-                # Create WKT geometry (postgis won't read shapely geometry)
-                urban_pop["geometry"] = [MultiPolygon([feature]) if type(feature) == Polygon else feature for feature in urban_pop["geometry"]]
-                urban_pop['geom'] = urban_pop['geometry'].apply(lambda x: WKTElement(x.wkt, srid=srid))
-                urban_pop.drop('geometry', 1, inplace=True)
-                # urban_pop.drop('sum', 1, inplace=True)
-                # Ensure all geometries are multipolygons (specifically - can't be mixed type; complicates things)
-                print(f"  - copying to postgis ({pop_feature})...")
-                # Copy to project Postgis database
-                urban_pop.to_sql(pop_feature, engine, if_exists='replace', index=True, dtype={'geom': Geometry('MULTIPOLYGON', srid=srid)})
-            else:
-                print(f"    - hex grid with population zonal statistics has already been procesed ({pop_feature}).")
+        db_contents = inspect(engine)
+        clipping_boundary = gpd.GeoDataFrame.from_postgis(
+            f'''SELECT geom FROM {buffered_study_region}''', 
+            engine, 
+            geom_col='geom' 
+            )   
+        # construct virtual raster table
+        vrt = os.path.join(population_folder,f'ghs_{p}.vrt')
+        population_raster_clipped   = os.path.join(locale_dir,f'ghs_{ghs_year}_{p}_{locale}.tif')
+        population_raster_projected = os.path.join(locale_dir,f'ghs_{ghs_year}_{p}_{locale}_{srid}.tif')
+        pop_feature = f'pop_ghs_{ghs_year}'
+        print("Global population dataset..."),
+        if not os.path.isfile(vrt):
+            tif_folder = f'../data/GHS/{p}'
+            tif_files = [os.path.join(tif_folder,file) for file in os.listdir(tif_folder) if os.path.splitext(file)[-1] == '.tif']
+            gdal.BuildVRT(vrt, tif_files)
+            print(f"  has now been indexed ({vrt}).")
+        else:
+            print(f"  has already been indexed ({vrt}).")
+        print("Population data clipped to region..."),
+        if not os.path.isfile(population_raster_clipped):
+            # extract study region boundary in projection of tiles
+            clipping = clipping_boundary.to_crs(epsg)
+            # get clipping boundary values in required order for gdal translate
+            bbox = list(clipping.bounds[['minx','maxy','maxx','miny']].values[0]) 
+            # bbox = list(clipping.bounds.values[0]) 
+            ds = gdal.Translate(population_raster_clipped, vrt, projWin = bbox)
+            ds = None
+            print(f"  has now been created ({population_raster_clipped}).")
+        else:
+            print(f"  has already been created ({population_raster_clipped}).")
+        print("Population data projected for region..."),
+        if not os.path.isfile(population_raster_projected):
+            # reproject and save the re-projected clipped raster
+            # (see config file for reprojection function)
+            reproject_raster(inpath = population_raster_clipped, 
+                          outpath = population_raster_projected, 
+                          new_crs = 'EPSG:{}'.format(srid))   
+            print(f"  has now been created ({population_raster_projected}).")
+        else:
+            print(f"  has already been created ({population_raster_projected}).")
+        print("Interpolation of population data to urban area...")
+        analysis_area = gpd.GeoDataFrame.from_postgis(region_sql,
+                                                        engine, 
+                                                        geom_col='geom')
+        print("  - processing population zonal statistics...")
+        pop_feature = "urban_study_region_pop"
+        if not db_contents.has_table(pop_feature):
+            result = zonal_stats(analysis_area,population_raster_projected,stats="sum", all_touched=True,geojson_out=True, nodata=-200)
+            print("  - creating additional required fields...")
+            urban_pop = gpd.GeoDataFrame.from_features(result)
+            urban_pop.rename(columns={'sum':'urban_pop_est'},inplace=True)
+            urban_pop['area_sqkm'] = urban_pop['geometry'].area/10**6
+            urban_pop['pop_per_sqkm'] = urban_pop['urban_pop_est'] / urban_pop['area_sqkm']
+            # Create WKT geometry (postgis won't read shapely geometry)
+            urban_pop["geometry"] = [MultiPolygon([feature]) if type(feature) == Polygon else feature for feature in urban_pop["geometry"]]
+            urban_pop['geom'] = urban_pop['geometry'].apply(lambda x: WKTElement(x.wkt, srid=srid))
+            urban_pop.drop('geometry', 1, inplace=True)
+            # urban_pop.drop('sum', 1, inplace=True)
+            # Ensure all geometries are multipolygons (specifically - can't be mixed type; complicates things)
+            print(f"  - copying to postgis ({pop_feature})...")
+            # Copy to project Postgis database
+            urban_pop.to_sql(pop_feature, engine, if_exists='replace', index=True, dtype={'geom': Geometry('MULTIPOLYGON', srid=srid)})
+        else:
+            print(f"    - hex grid with population zonal statistics has already been procesed ({pop_feature}).")
     else:
         print(locale)
         xls = pandas.ExcelFile(os.path.join(cwd,'_project_configuration.xls'))
@@ -139,7 +142,7 @@ def main():
         for var in [x for x in  df_local.index.values]:
             globals()[var] = df_local.loc[var][locale]
         study_region = '{}_{}_{}'.format(locale,region,year).lower()
-        vic_region_sql = f'''
+        other_region_sql = f'''
         SELECT "Study region" study_region, 
                geom
         FROM {study_region}
@@ -148,69 +151,71 @@ def main():
         locale_dir = os.path.join(folder_path,'study_region','{}'.format(study_region))
         buffered_study_region = '{}_{}{}'.format(study_region,study_buffer,units)
         engine = create_engine(f"postgresql://{db_user}:{db_pwd}@{db_host}/{db}")
-    db_contents = inspect(engine)
-        clipping_boundary = gpd.GeoDataFrame.from_postgis('''SELECT geom FROM {table}'''.format(table = buffered_study_region), engine, geom_col='geom' )   
-        for p in projections:
-            # construct virtual raster table
-            vrt = os.path.join(population_folder,f'ghs_{p}.vrt')
-            population_raster_clipped   = os.path.join(locale_dir,f'ghs_{ghs_year}_{p}_{locale}.tif')
-            population_raster_projected = os.path.join(locale_dir,f'ghs_{ghs_year}_{p}_{locale}_{srid}.tif')
-            pop_feature = f'pop_ghs_{ghs_year}'
-            print("Global population dataset..."),
-            if not os.path.isfile(vrt):
-                tif_folder = f'../data/GHS/{p}'
-                tif_files = [os.path.join(tif_folder,file) for file in os.listdir(tif_folder) if os.path.splitext(file)[-1] == '.tif']
-                gdal.BuildVRT(vrt, tif_files)
-                print(f"  has now been indexed ({vrt}).")
-            else:
-                print(f"  has already been indexed ({vrt}).")
-            print("Population data clipped to region..."),
-            if not os.path.isfile(population_raster_clipped):
-                # extract study region boundary in projection of tiles
-                clipping = clipping_boundary.to_crs({'init':'epsg:{}'.format(projections[p])})
-                # get clipping boundary values in required order for gdal translate
-                bbox = list(clipping.bounds[['minx','maxy','maxx','miny']].values[0]) 
-                # bbox = list(clipping.bounds.values[0]) 
-                ds = gdal.Translate(population_raster_clipped, vrt, projWin = bbox)
-                ds = None
-                print(f"  has now been created ({population_raster_clipped}).")
-            else:
-                print(f"  has already been created ({population_raster_clipped}).")
-            print("Population data projected for region..."),
-            if not os.path.isfile(population_raster_projected):
-                # reproject and save the re-projected clipped raster
-                # (see config file for reprojection function)
-                reproject_raster(inpath = population_raster_clipped, 
-                              outpath = population_raster_projected, 
-                              new_crs = 'EPSG:{}'.format(srid))   
-                print(f"  has now been created ({population_raster_projected}).")
-            else:
-                print(f"  has already been created ({population_raster_projected}).")
-            print("Interpolation of population data to urban area...")
-            analysis_area = gpd.GeoDataFrame.from_postgis(vic_region_sql,
-                                                            engine, 
-                                                            geom_col='geom')
-            print("  - processing population zonal statistics...")
-            pop_feature = "urban_study_region_pop"
-            if not db_contents.has_table(pop_feature):
-                result = zonal_stats(analysis_area,population_raster_projected,stats="sum", all_touched=True,geojson_out=True, nodata=-200)
-                print("  - creating additional required fields...")
-                urban_pop = gpd.GeoDataFrame.from_features(result)
-                urban_pop.rename(columns={'sum':'urban_pop_est'},inplace=True)
-                urban_pop['area_sqkm'] = urban_pop['geometry'].area/10**6
-                urban_pop['pop_per_sqkm'] = urban_pop['urban_pop_est'] / urban_pop['area_sqkm']
-                # Create WKT geometry (postgis won't read shapely geometry)
-                urban_pop["geometry"] = [MultiPolygon([feature]) if type(feature) == Polygon else feature for feature in urban_pop["geometry"]]
-                urban_pop['geom'] = urban_pop['geometry'].apply(lambda x: WKTElement(x.wkt, srid=srid))
-                urban_pop.drop('geometry', 1, inplace=True)
-                # urban_pop.drop('sum', 1, inplace=True)
-                # Ensure all geometries are multipolygons (specifically - can't be mixed type; complicates things)
-                print(f"  - copying to postgis ({pop_feature})...")
-                # Copy to project Postgis database
-                urban_pop.to_sql(pop_feature, engine, if_exists='replace', index=True, dtype={'geom': Geometry('MULTIPOLYGON', srid=srid)})
-            else:
-                print(f"    - hex grid with population zonal statistics has already been procesed ({pop_feature}).")
-    
+        db_contents = inspect(engine)
+        clipping_boundary = gpd.GeoDataFrame.from_postgis(
+            f'''SELECT geom FROM {buffered_study_region}''', 
+            engine, 
+            geom_col='geom' )   
+        # construct virtual raster table
+        vrt = os.path.join(population_folder,f'ghs_{p}.vrt')
+        population_raster_clipped   = os.path.join(locale_dir,f'ghs_{ghs_year}_{p}_{locale}.tif')
+        population_raster_projected = os.path.join(locale_dir,f'ghs_{ghs_year}_{p}_{locale}_{srid}.tif')
+        pop_feature = f'pop_ghs_{ghs_year}'
+        print("Global population dataset..."),
+        if not os.path.isfile(vrt):
+            tif_folder = f'../data/GHS/{p}'
+            tif_files = [os.path.join(tif_folder,file) for file in os.listdir(tif_folder) if os.path.splitext(file)[-1] == '.tif']
+            gdal.BuildVRT(vrt, tif_files)
+            print(f"  has now been indexed ({vrt}).")
+        else:
+            print(f"  has already been indexed ({vrt}).")
+        print("Population data clipped to region..."),
+        if not os.path.isfile(population_raster_clipped):
+            # extract study region boundary in projection of tiles
+            clipping = clipping_boundary.to_crs({'init':'epsg:{}'.format(projections[p])})
+            # get clipping boundary values in required order for gdal translate
+            bbox = list(clipping.bounds[['minx','maxy','maxx','miny']].values[0]) 
+            # bbox = list(clipping.bounds.values[0]) 
+            ds = gdal.Translate(population_raster_clipped, vrt, projWin = bbox)
+            ds = None
+            print(f"  has now been created ({population_raster_clipped}).")
+        else:
+            print(f"  has already been created ({population_raster_clipped}).")
+        print("Population data projected for region..."),
+        if not os.path.isfile(population_raster_projected):
+            # reproject and save the re-projected clipped raster
+            # (see config file for reprojection function)
+            reproject_raster(inpath = population_raster_clipped, 
+                          outpath = population_raster_projected, 
+                          new_crs = 'EPSG:{}'.format(srid))   
+            print(f"  has now been created ({population_raster_projected}).")
+        else:
+            print(f"  has already been created ({population_raster_projected}).")
+        print("Interpolation of population data to urban area...")
+        analysis_area = gpd.GeoDataFrame.from_postgis(other_region_sql,
+                                                        engine, 
+                                                        geom_col='geom')
+        print("  - processing population zonal statistics...")
+        pop_feature = "urban_study_region_pop"
+        if not db_contents.has_table(pop_feature):
+            result = zonal_stats(analysis_area,population_raster_projected,stats="sum", all_touched=True,geojson_out=True, nodata=-200)
+            print("  - creating additional required fields...")
+            urban_pop = gpd.GeoDataFrame.from_features(result)
+            urban_pop.rename(columns={'sum':'urban_pop_est'},inplace=True)
+            urban_pop['area_sqkm'] = urban_pop['geometry'].area/10**6
+            urban_pop['pop_per_sqkm'] = urban_pop['urban_pop_est'] / urban_pop['area_sqkm']
+            # Create WKT geometry (postgis won't read shapely geometry)
+            urban_pop["geometry"] = [MultiPolygon([feature]) if type(feature) == Polygon else feature for feature in urban_pop["geometry"]]
+            urban_pop['geom'] = urban_pop['geometry'].apply(lambda x: WKTElement(x.wkt, srid=srid))
+            urban_pop.drop('geometry', 1, inplace=True)
+            # urban_pop.drop('sum', 1, inplace=True)
+            # Ensure all geometries are multipolygons (specifically - can't be mixed type; complicates things)
+            print(f"  - copying to postgis ({pop_feature})...")
+            # Copy to project Postgis database
+            urban_pop.to_sql(pop_feature, engine, if_exists='replace', index=True, dtype={'geom': Geometry('MULTIPOLYGON', srid=srid)})
+        else:
+            print(f"    - hex grid with population zonal statistics has already been procesed ({pop_feature}).")
+
     # grant access to the tables just created
     engine.execute(grant_query)                                                                  
     df = pandas.read_sql(count_sql, con=engine, index_col=None)
