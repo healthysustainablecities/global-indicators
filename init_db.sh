8788c708d82224b12976f51f0a502688b5ae1c7c
Carl Higgs <carlhiggs@gmail.com>
2022-12-05 16:44:08 +1100

    updated init_db.sh to not explicitly refer to host ip (user must configure this in any case in config.yml, and may differ by system, as per #162

--- init_db.sh
+++ init_db.sh
@@ -1,2 +1,2 @@
 docker pull pgrouting/pgrouting
-docker run --name=postgis -d -e POSTGRES_PASSWORD=ghscic -p 127.0.0.1:5433:5432 --restart=unless-stopped --volume=/var/lib/postgis:/postgresql/13/main pgrouting/pgrouting
+docker run --name=postgis -d -e POSTGRES_PASSWORD=ghscic -p 5433:5432 --restart=unless-stopped --volume=/var/lib/postgis:/postgresql/13/main pgrouting/pgrouting
--- win-docker-bash.bat
+++
@@ -1,3 +0,0 @@
-docker pull globalhealthyliveablecities/global-indicators:latest
-git pull
-docker run --rm -it --name=global-indicators --shm-size=2g --net=host -v "%cd%":/home/jovyan/work globalhealthyliveablecities/global-indicators /bin/bash
