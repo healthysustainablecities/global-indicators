0b9c5f43cc3e3b5c188868b88c08adce7f1827f8
Carl Higgs <carlhiggs@gmail.com>
2023-04-17 18:43:07 +1000

    updated the docker compose file to launch instructions after loading and before bash; requires a sleep -- need to test this to see if its too variable to be stable; but it looks nicer/more informative for user

--- global-indicators.bat
+++ global-indicators.bat
@@ -7,15 +7,6 @@ echo.
 echo https://healthysustainablecities.org ('1000 Cities Challenge')
 echo https://global-healthy-liveable-cities.github.io/ (Software guide)
 echo.
-echo To run the provided example analysis for Las Palmas, Spain, enter:
-echo python 1_create_project_configuration_files.py example_ES_Las_Palmas_2023
-echo python 2_analyse_region.py example_ES_Las_Palmas_2023
-echo python 3_generate_resources.py example_ES_Las_Palmas_2023
-echo.
-echo For more directions on each step, run the same command without including the study region codename.
-echo.
-echo To exit, type and enter: exit
-echo.
 :: check if argument is equal to string "jupyter"
 if "%~1"=="jupyter" ((
 :: if Docker exists and is runnable, launch the Docker compose image
--- global-indicators.sh
+++ global-indicators.sh
@@ -1,19 +1,10 @@
 echo
 echo Global Healthy and Sustainable City Indicators
-VERSION=$(cat "$PWD/.ghsci_version")
-cat "$VERSION"
+VERSION="$(cat "$PWD/.ghsci_version")"
+echo "$VERSION"
 echo
 echo https://healthysustainablecities.org \('1000 Cities Challenge'\)
 echo https://global-healthy-liveable-cities.github.io/ \(Software guide\)
-echo
-echo To run the provided example analysis for Las Palmas, Spain, enter:
-echo python 1_create_project_configuration_files.py example_ES_Las_Palmas_2023
-echo python 2_analyse_region.py example_ES_Las_Palmas_2023
-echo python 3_generate_resources.py example_ES_Las_Palmas_2023
-echo
-echo For more directions on each step, without specifying a study region.
-echo
-echo To exit, type and enter: exit
 echo
 if [[ "$*" == *"jupyter"* ]]
 then
