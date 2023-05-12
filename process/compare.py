cfa0be34b10be5cc922e1a52d56782e0a7321759
Carl Higgs <carlhiggs@gmail.com>
2023-05-12 10:44:48 +1000

    updated the compare script to advise users when results are identical for two compared regions.

--- process/compare.py
+++ process/compare.py
@@ -20,7 +20,6 @@ def check_codenames(codename, comparison_codename):
             sys.exit(
                 f"""Compare a reference city to a comparison city, and save the comparison as a CSV file.\n\nSpecified city ({name}) does not appear to be in the list of configured cities.\n\nPlease try again by entering codenames from the list of configured cities {region_names} that have been fully analysed with resources generated:\npython 4_compare.py <reference> <comparison>\n\nAlternatively, enter the shortcut command:\ncompare <reference> <comparison>""",
             )
-
     if comparison_codename == codename:
         sys.exit(
             f"""Compare a reference city to a comparison city, and save the comparison as a CSV file.\n\nThe same codename was provided as reference and comparison.  This process is designed to summarise differences, and there would be none in this case.\n\nPlease try again by selecting two different codenames from the list of configured cities {region_names}, where these are study regions that have been fully analysed with resources generated.\n\nThe command can be run by entering:\ncompare  <reference> <comparison>\n\nAlternatively, enter the shortcut command:\ncompare <reference> <comparison>""",
@@ -28,7 +27,7 @@ def check_codenames(codename, comparison_codename):
 
 
 def compare(codename, comparison_codename):
-    """Given a Region object (r) and a comparison codename for two cities with generated resources, compare the two cities and save the comparison as a CSV file."""
+    """Given a codename and a comparison codename for two cities with generated resources, compare the two cities and save the comparison as a CSV file."""
     r = Region(codename)
     files = {
         'reference': f"{r.config['region_dir']}/{r.config['city_summary']}.csv",
@@ -36,9 +35,7 @@ def compare(codename, comparison_codename):
             r.codename, comparison_codename,
         ),
     }
-
     dfs = {}
-
     for file in files:
         if os.path.exists(files[file]):
             dfs[file] = pd.read_csv(files[file])
@@ -46,16 +43,19 @@ def compare(codename, comparison_codename):
             sys.exit(
                 f"""Compare a reference city to a comparison city, and save the comparison as a CSV file.\n\nThe summary results file ({files[file]}) could not be located.\n\nPlease try again by entering codenames from the list of configured cities {region_names} that have been fully analysed with resources generated:\npython 4_compare.py <reference> <comparison>\n\nAlternatively, enter the shortcut command:\ncompare <reference> <comparison>""",
             )
-
-    comparison = (
-        dfs['reference']
-        .compare(dfs['comparison'], align_axis=0)
-        .droplevel(0)
-        .transpose()
-    )
+    if all(dfs['reference'] == dfs['comparison']):
+        sys.exit(
+            f'The results contained in the generated summaries for {codename} and {comparison_codename} are identical.',
+        )
+    else:
+        comparison = (
+            dfs['reference']
+            .compare(dfs['comparison'], align_axis=0)
+            .droplevel(0)
+            .transpose()
+        )
     comparison.columns = [r.codename, comparison_codename]
     print(comparison)
-
     comparison.to_csv(
         f"{r.config['region_dir']}/compare_{r.codename}_{comparison_codename}_{date_hhmm}.csv",
     )
