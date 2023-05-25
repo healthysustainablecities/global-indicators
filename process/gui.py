e11f5ee305f43bd6a521a60a44dcdebbff586c1d
Carl Higgs <carlhiggs@gmail.com>
2023-05-25 12:06:39 +1000

    updated gui.py draft layout (removed split screen for study region selection; now its own tab, with study region name header now using the @ui.refreshable decorator

--- process/gui.py
+++ process/gui.py
@@ -26,6 +26,10 @@ class Region:
         self.year = ''
         self.configured = ticks[False]
         self.config = {}
+        self.config[
+            'header_name'
+        ] = 'Select or create a new codename representing a study region to view region configuration details'
+        self.config['notes'] = ''
 
 
 def get_config_string(codename: str) -> str:
@@ -50,7 +54,9 @@ def get_locations() -> dict:
                     'name': r.name,
                     'country': r.config['country'],
                     'year': str(r.config['year']),
+                    'notes': r.config['notes'],
                     'configured': ticks[True],
+                    'config': r.config,
                 },
             )
         except:
@@ -61,9 +67,15 @@ def get_locations() -> dict:
                     'name': '',
                     'country': '',
                     'year': '',
+                    'notes': '',
                     'configured': ticks[False],
+                    'config': {
+                        'header_name': f'{codename} (configuration not yet complete)',
+                        'notes': 'Region configuration file could not be loaded and requires completion in a text editor.',
+                    },
                 },
             )
+            locations[-1]['config']['name'] = ''
     return locations
 
 
@@ -74,12 +86,19 @@ def set_region_codename(selection: list) -> None:
         region.country = ''
         region.year = ''
         region.configured = ticks[False]
+        region.config = {}
+        region.config[
+            'header_name'
+        ] = 'Select or create a new codename representing a study region to view region configuration details'
     else:
         region.codename = selection[0]['codename']
         region.name = selection[0]['name']
         region.country = selection[0]['country']
         region.year = selection[0]['year']
         region.configured = selection[0]['configured']
+        region.config = selection[0]['config']
+        region.config['header_name'] = load_configuration_text(selection)
+    studyregion_ui.refresh()
 
 
 def load_configuration_text(selection: list) -> str:
@@ -104,14 +123,36 @@ def try_function(
         ui.notify(fail_message)
 
 
+@ui.refreshable
+def studyregion_ui() -> None:
+    ui.html(region.config['header_name']).style(
+        'color: #6E93D6; font-size: 123%; font-weight: 500',
+    )
+
+
 ticks = ['✘', '✔']
 region = Region()
 locations = get_locations()
+ghsci.datasets.pop('dictionary', None)
 columns = [
+    {
+        'name': 'name',
+        'label': 'Name',
+        'field': 'name',
+        'sortable': True,
+        'required': True,
+    },
     {
         'name': 'codename',
         'label': 'Codename',
         'field': 'codename',
+        'sortable': True,
+        'required': True,
+    },
+    {
+        'name': 'year',
+        'label': 'Year',
+        'field': 'year',
         'sortable': True,
         'required': True,
     },
@@ -131,11 +172,26 @@ ui.label(
 ).style('color: #6E93D6; font-size: 200%; font-weight: 300')
 
 
+# ui.html().bind_content_from(
+#     table,
+#     'selected',
+#     lambda val: load_configuration_text(val),
+# ).style('color: #6E93D6; font-size: 123%; font-weight: 500')
+studyregion_ui()
+
 ## Body
-with ui.splitter(value='500px') as splitter:
-    with splitter.before:
+with ui.tabs().props('align="left"') as tabs:
+    ui.tab('Study regions', icon='language')
+    ui.tab('Configure', icon='build')
+    ui.tab('Analysis', icon='data_thresholding')
+    ui.tab('Generate', icon='perm_media')
+    ui.tab('Compare', icon='balance')
+with ui.tab_panels(tabs, value='Study regions'):
+    with ui.tab_panel('Study regions'):
+        # with ui.splitter(value='500px') as splitter:
+        #     with splitter.before:
         with ui.table(
-            title='Study regions',
+            title='Select or create a new study region',
             columns=columns,
             rows=locations,
             pagination=10,
@@ -184,83 +240,74 @@ with ui.splitter(value='500px') as splitter:
                             ui.tooltip(
                                 'e.g. AU_Melbourne_2023 is a codename for the city of Melbourne, Australia in 2023',
                             ).style('color: white;background-color: #6e93d6;')
-    with splitter.after:
-        with ui.tabs().props('align="left"') as tabs:
-            ui.tab('Configure', icon='build')
-            ui.tab('Analysis', icon='data_thresholding')
-            ui.tab('Generate', icon='perm_media')
-            ui.tab('Compare', icon='balance')
-        with ui.tab_panels(tabs, value='Configure'):
-            with ui.tab_panel('Configure'):
-                ui.html().bind_content_from(
-                    table,
-                    'selected',
-                    lambda val: load_configuration_text(val),
-                ).style('color: #6E93D6; font-size: 123%; font-weight: 500')
-                # with ui.expansion('Region settings'):
-                #     ui.markdown().bind_content_from(
-                #         table,
-                #         'selected',
-                #         lambda val: load_configuration_text(val, True),
-                #     )
-                with ui.expansion('Datasets'):
-                    ui.markdown(
-                        f'Define shared datasets for use in your project using configuration/datasets.yml:\n\n```{ghsci.datasets}```',
-                    )
-                with ui.expansion('Advanced settings'):
-                    with ui.expansion('Reporting languages and templates'):
-                        ui.markdown(
-                            'Edit settings in configuration/_report_configuration.xlsx',
-                        )
-                    with ui.expansion('Project'):
-                        ui.markdown(
-                            f'Edit the following project settings in configuration/config.yml:\n\n```{ghsci.settings}```',
-                        )
-                    with ui.expansion(
-                        'OpenStreetMap-derived Areas of Open Space',
-                    ):
-                        ui.markdown(
-                            f'Edit settings in configuration/osm_open_space.yml:\n\n```{ghsci.osm_open_space}```',
-                        )
-                    with ui.expansion('Indicators'):
-                        ui.markdown(
-                            f'Edit settings in configuration/indicators.yml:\n\n```{ghsci.indicators}```',
-                        )
-                    with ui.expansion('Policies'):
-                        ui.markdown(
-                            f'Edit settings in configuration/policies.yml:\n\n```{ghsci.policies}```',
-                        )
-            with ui.tab_panel('Analysis'):
-                ui.label(
-                    'Click the button below to run the analysis workflow.  Progress can be monitored from your terminal window, however this user interface may not respond until processing is complete.',
+            # with splitter.after:
+            # To do: insert study region map here, for where study region boundaries have been configured and loaded
+    with ui.tab_panel('Configure'):
+        ui.label(
+            'Project configuration details are summarised below.  It is recommended to view and modify these details using a text editor.',
+        )
+        # with ui.expansion('Region settings'):
+        #     ui.markdown().bind_content_from(
+        #         table,
+        #         'selected',
+        #         lambda val: load_configuration_text(val, True),
+        #     )
+        with ui.expansion('Datasets'):
+            ui.markdown(
+                f'Define shared datasets for use in your project using configuration/datasets.yml:\n\n```{ghsci.datasets}```',
+            )
+        with ui.expansion('Advanced settings'):
+            with ui.expansion('Reporting languages and templates'):
+                ui.markdown(
+                    'Edit settings in configuration/_report_configuration.xlsx',
                 )
-                ui.button(
-                    'Perform study region analysis',
-                    on_click=lambda: try_function(analysis, [region.codename]),
+            with ui.expansion('Project'):
+                ui.markdown(
+                    f'Edit the following project settings in configuration/config.yml:\n\n```{ghsci.settings}```',
                 )
-            with ui.tab_panel('Generate'):
-                ui.label(
-                    'Click the button below to generate project documentation and resources (data, images, maps, reports, etc).  More information on the outputs is displayedin the terminal window.',
+            with ui.expansion('OpenStreetMap-derived Areas of Open Space'):
+                ui.markdown(
+                    f'Edit settings in configuration/osm_open_space.yml:\n\n```{ghsci.osm_open_space}```',
                 )
-                ui.button(
-                    'Generate resources',
-                    on_click=lambda: try_function(generate, [region.codename]),
+            with ui.expansion('Indicators'):
+                ui.markdown(
+                    f'Edit settings in configuration/indicators.yml:\n\n```{ghsci.indicators}```',
                 )
-            with ui.tab_panel('Compare'):
-                ui.label(
-                    'To compare two study regions with generated resources.  Select a reference region in on left panel and a comparison region below:',
+            with ui.expansion('Policies'):
+                ui.markdown(
+                    f'Edit settings in configuration/policies.yml:\n\n```{ghsci.policies}```',
                 )
-                comparisons = ui.select(
-                    ghsci.region_names,
-                    with_input=True,
-                    value='Select comparison codename',
-                )
-                ui.button(
-                    'Compare study regions',
-                    on_click=lambda: try_function(
-                        compare, [region.codename, comparisons.value],
-                    ),
-                )
+    with ui.tab_panel('Analysis'):
+        ui.label(
+            'Click the button below to run the analysis workflow.  Progress can be monitored from your terminal window, however this user interface may not respond until processing is complete.',
+        )
+        ui.button(
+            'Perform study region analysis',
+            on_click=lambda: try_function(analysis, [region.codename]),
+        )
+    with ui.tab_panel('Generate'):
+        ui.label(
+            'Click the button below to generate project documentation and resources (data, images, maps, reports, etc).  More information on the outputs is displayedin the terminal window.',
+        )
+        ui.button(
+            'Generate resources',
+            on_click=lambda: try_function(generate, [region.codename]),
+        )
+    with ui.tab_panel('Compare'):
+        ui.label(
+            'To compare two study regions with generated resources.  Select a reference region in on left panel and a comparison region below:',
+        )
+        comparisons = ui.select(
+            ghsci.region_names,
+            with_input=True,
+            value='Select comparison codename',
+        )
+        ui.button(
+            'Compare study regions',
+            on_click=lambda: try_function(
+                compare, [region.codename, comparisons.value],
+            ),
+        )
 
 
 with ui.dialog() as dialog, ui.card():
