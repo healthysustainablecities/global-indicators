77a05e43f203328e5e2976bf4718b22b17e1d9dd
Carl Higgs <carlhiggs@gmail.com>
2023-06-02 22:06:19 +1000

    added in leaflet helper scripts for gui map draft; moving towards some kind of interactive map offering as per #221

---
+++ process/subprocesses/leaflet.js
@@ -0,0 +1,29 @@
+export default {
+  template: "<div></div>",
+  mounted() {
+    this.map = L.map(this.$el);
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+    }).addTo(this.map);
+  },
+  methods: {
+    set_location(latitude, longitude, zoom = 9) {
+      this.target = L.latLng(latitude, longitude);
+      this.map.setView(this.target, zoom);
+      if (this.marker) {
+        this.map.removeLayer(this.marker);
+      }
+      this.marker = L.marker(this.target);
+      this.marker.addTo(this.map);
+      console.log("set_location", latitude, longitude, zoom);
+    },
+    set_no_location(latitude, longitude, zoom = 9) {
+      this.target = L.latLng(latitude, longitude);
+      this.map.setView(this.target, zoom);
+      if (this.marker) {
+        this.map.removeLayer(this.marker);
+      }
+      console.log("set_location", latitude, longitude, zoom);
+    },
+  },
+};
---
+++ process/subprocesses/leaflet.py
@@ -0,0 +1,36 @@
+"""Leaflet helper script for GHSCI map, drawing on NiceGUI example."""
+
+from typing import Tuple
+
+from nicegui import ui
+from nicegui.dependencies import register_component
+from nicegui.element import Element
+
+register_component('leaflet', __file__, 'leaflet.js')
+
+
+class leaflet(Element):
+    """Leaflet helper script for GHSCI map, drawing on NiceGUI example."""
+
+    def __init__(self) -> None:
+        super().__init__('leaflet')
+        ui.add_head_html(
+            """<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>""",
+        )
+        ui.add_head_html(
+            """<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>""",
+        )
+
+    def set_location(self, location: Tuple[float, float], zoom: int) -> None:
+        """Place marker and set the map location and zoom level."""
+        self.run_method('set_location', location[0], location[1], zoom)
+
+    def set_no_location(
+        self, location: Tuple[float, float], zoom: int,
+    ) -> None:
+        """Set the map location and zoom level, without placing a marker."""
+        self.run_method('set_no_location', location[0], location[1], zoom)
