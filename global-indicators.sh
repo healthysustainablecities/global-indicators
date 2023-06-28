2ac046f0df1097be99a258c29c1d006c3e2c4ac0
Carl Higgs <carlhiggs@gmail.com>
2023-06-28 15:09:18 +1000

    updated Docker image and associated scripts, and removed the jupyter notebook specific docker compose file (not required; jupyter lab can now be launched from the main ghsci interface); simplified the global-indicators .bat and .sh scripts to reflect this simpler logic

--- global-indicators.sh
+++ global-indicators.sh
@@ -6,23 +6,11 @@ echo
 echo https://healthysustainablecities.org \('1000 Cities Challenge'\)
 echo https://global-healthy-liveable-cities.github.io/ \(Software guide\)
 echo
-if [[ "$*" == *"jupyter"* ]]
-then
-    { # try
-        # if Docker exists and is runnable, launch the Docker compose image
-        docker compose -f docker-compose-jupyter.yml up -d
-        # attach to the GHSCI software process
-        docker attach ghsci
-    } || { # catch
-        echo "Please ensure that Docker Desktop is installed and running (https://www.docker.com/products/docker-desktop/). Docker Desktop includes Docker Compose, which is required to run this software."
-    }
-else
-    { # try
-        # if Docker exists and is runnable, launch the Docker compose image
-        docker compose -f docker-compose.yml up -d
-        # attach to the GHSCI software process
-        docker attach ghsci
-    } || { # catch
-        echo "Please ensure that Docker Desktop is installed and running (https://www.docker.com/products/docker-desktop/). Docker Desktop includes Docker Compose, which is required to run this software."
-    }
-fi
+{ # try
+    # if Docker exists and is runnable, launch the Docker compose image
+    docker compose -f docker-compose.yml up -d
+    # attach to the GHSCI software process
+    docker attach ghsci
+} || { # catch
+    echo "Please ensure that Docker Desktop is installed and running (https://www.docker.com/products/docker-desktop/). Docker Desktop includes Docker Compose, which is required to run this software."
+}
