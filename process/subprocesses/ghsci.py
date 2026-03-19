8ec9a755b3743a70b24543843162169029ddce93
Carl Higgs <carl.higgs@rmit.edu.au>
2026-03-19 17:44:26 +1100

    removed policies statement rom policy_data_setup call in get_policy_checklist function in ghsci.py towards #597

--- process/subprocesses/ghsci.py
+++ process/subprocesses/ghsci.py
@@ -2199,8 +2199,7 @@ class Region:
             )
         else:
             return policy_data_setup(
-                policy_review_xlsx_path,
-                policies,
+                policy_review_xlsx_path
             )
 
     def get_scorecard_statistics(self, export=False):
